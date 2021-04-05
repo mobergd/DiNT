c622207f0cbc01d1c5f7f7aa3bec7cd432559b2e
mobergd <mobergdr@gmail.com>
2021-04-05 12:34:41 -0500

    Fixed typo in src file headers

--- src/c_sys.f
+++ src/c_sys.f
@@ -15,7 +15,7 @@ c
 c  version 2.0                                    
 c
 c  A. W. Jasper                  
-c  Argonne National Laboratories     
+c  Argonne National Laboratory     
 c
 c  Rui Ming Zhang                 
 c  Tsinghua University
